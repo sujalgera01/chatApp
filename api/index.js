b1879ba6d0643703d640edf4c5f74d24db0eadb0
sujalgera01 <sujalgera01@gmail.com>
2023-11-23 15:10:05 +0530

    my first commit

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const User = require('./models/User')
+const jwt = require('jsonwebtoken')
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
+const app = express();
+
+app.get('/test', (req,res)=>{
+    res.json('test ok')
+});
+
+app.post('/register', async (req,res)=>{
+    const {username, password} = req.body;
+    const createdUser = await User.create({username,password});
+    jwt.sign({userId: createdUser, _id}, jwtSecret, (err,token)=>{
+        if(err) throw err;
+        res.cookie('token', token).status(201).json('ok');
+    })
+})
+
+app.listen(4040);
---
+++ client/src/Register.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+  return (
+    <div className='bg-blue-50 h-screen flex items-center'>
+        <form className='w-64 mx-auto mb-12'>
+            <input value={username} onChange={ev => setUsername(ev.target.value)} type="text" placeholder='username' className="block w-full rounded-sm mb-2" />
+            <input value={password} onChange={ev => setPassword(ev.target.value)} type="password" placeholder='password' className="block w-full rounded-sm mb-2 border"/>
+            <button className='bg-blue-500 text-white block w-full rounded-sm p-2'>Register</button>
+        </form>
+    </div>
+  )
+}
+
+export default Register
